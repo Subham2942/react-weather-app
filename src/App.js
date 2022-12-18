6f4487d3a89b9b6c7ba33fdd16f40ea4f0d085f2
Subham Das <subhamdassneha@gmail.com>
2022-12-19 03:48:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,71 +5,55 @@ import Inputs from './Components/Inputs';
 import TimeAndLocation from './Components/TimeAndLocation';
 import TemperatureAndDetails from './Components/TemperatureAndDetails';
 import Forecast from './Components/Forecast';
-import getFormattedWeatherData from './Services/weatherService'
 import { useEffect, useState } from 'react';
 import getWeatherData from './Services/weatherService';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {FaSadTear} from 'react-icons/fa'
 
 function App() {
 
-  const [query, setQuery] = useState("Rourkela");
+  const [query, setQuery] = useState("Tokyo");
   const [unit, setUnit ] = useState("metric");
   const [weather, setWeather] = useState(null);
 
-  // const fetchWeather = async()=>{
-  //   const data = await getWeatherData( 'forecast', {q: "Rourkela"});
-  //   console.log(data);
-  // }
-
-  // fetchWeather();
 
   useEffect(()=> {
     const fetchWeather = async()=>{
-      const data = await getWeatherData( 'forecast', {q: "New York"});
-      setWeather(data);
-
-      
+      toast.info("Fetching Weather...")
+      await getWeatherData( 'forecast', {q: query})
+      .then(data => {
+        toast.success(`Successfully fetched weather for ${data.location.name}` )
+        setWeather(data)
+      })
+      .catch(err=>{
+        toast.error("City Not Found!");
+        setWeather(null);
+      })
     }
     fetchWeather();
   }, [query]);
-  console.log(weather);
  
 
-  // useEffect(() =>{
-  //   const fetchWeather = async()=>{
-  //       await getFormattedWeatherData({...query})
-  //       .then(data =>{
-  //         setWeather(data);
-  //       })
-  //       // console.log(data);
-  //     }
-  // }, [query, unit]);
-
-
-  // useEffect(()=>{
-  //   const fetchWeather = async() =>{
-  //     const data = await getWeatherData('forecast', {q: "Rourkela"})
-  //     setWeather(data);
-  //   }
-  //   console.log(weather);
-  // }, [query])
+  
   
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 
-    bg-gradient-to-br from-cyan-700 to-blue-700 h-fit 
+    bg-gradient-to-br from-violet-700 to-red-700 h-fit 
     shadow-xl shadow-gray-400">
-      <TopButtons/>
-      <Inputs />
+      <TopButtons setQuery={setQuery} />
+      <Inputs unit={unit} setUnit={setUnit} setQuery={setQuery} />
 
-      { weather && (
+      { weather ?  (
         <div>
             <TimeAndLocation weather={weather}  />
-            <TemperatureAndDetails weather={weather} units={unit}/>
-            <Forecast title={"Hourly Forecast"} weather={weather} />
-            <Forecast title={"Daily Forecast"} weather={weather} />
+            <TemperatureAndDetails weather={weather} unit={unit}/>
+            <Forecast title={"Hourly Forecast"} weather={weather} unit = {unit} />
         </div>
-      )}
+      ) : <h1 className="text-white font-thin text-3xl text-center my-10 flex justify-center gap-5 ">City not found <span><FaSadTear/></span> </h1>   }
       
- 
+      <ToastContainer autoClose={1500} theme={"colored"} newestOnTop />
       </div>
   );
 }
