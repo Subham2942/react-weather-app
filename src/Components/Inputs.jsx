41f8bb78977d5dc5127776c64ffcb0b4d17e6760
Subham Das <subhamdassneha@gmail.com>
2022-12-19 03:49:14 +0530

    Update Inputs.jsx

--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -2,32 +2,58 @@ import React from "react";
 
 import { FaSearch } from "react-icons/fa";
 import { ImLocation } from "react-icons/im";
+import { useState } from "react";
 
-const Inputs = () => {
+const Inputs = ({ unit, setUnit, setQuery }) => {
+  const [city, setCity] = useState("");
+
+  const handleSearchClick = e => {
+    e.preventDefault();
+    if (city !== "") setQuery(city);
+    setCity("");
+  };
   return (
     <div className="flex felx-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
-        <input
-          type="text"
-          placeholder="search for city..."
-          className="text-xl p-2 font-light w-full shadow-xl outline-none capitalize placeholder:lowercase"
-        />
-        <FaSearch
-          size={25}
-          className="text-white cursor-pointer transition ease-out hover:scale-125"
-        />
-        <ImLocation
-          size={25}
-          className="text-white cursor-pointer transition ease-out hover:scale-125"
-        />
+
+        <form action="" className="flex justify-around w-full" onSubmit={handleSearchClick}>
+          <input
+            value={city}
+            onChange={(e) => setCity(e.currentTarget.value)}
+            type="text"
+            placeholder="search for city..."
+            className="text-xl p-2 font-light w-10/12 shadow-xl outline-none capitalize placeholder:lowercase "
+          />
+          <button type="submit" className="text-white cursor-pointer transition ease-out hover:scale-125">
+            <FaSearch size={20}/>
+          </button>
+        </form>
+        
+        
       </div>
       <div className="flex flex-row w-1/4 items-center justify-center">
-        <button name="metric" className="text-xl text-white font-light">
-            °C
+        <button
+          name="metric"
+          onClick={() => setUnit("metric")}
+          className={
+            unit === "metric"
+              ? `text-xl text-white font-bold`
+              : `text-xl text-white font-light`
+          }
+        >
+          °C
         </button>
-        <p className="text-white text-xl mx-1" >|</p>
-        <button name="imperial" className="text-xl text-white font-light">
-            °F
+        <p className="text-white text-xl mx-1">|</p>
+        <button
+          name="imperial"
+          onClick={() => setUnit("imperial")}
+          className={
+            unit === "imperial"
+              ? `text-xl text-white font-bold`
+              : `text-xl text-white font-light`
+          }
+        >
+          °F
         </button>
       </div>
     </div>
