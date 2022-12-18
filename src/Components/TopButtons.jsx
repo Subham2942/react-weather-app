a882462ab979c47d94640e196f43b767db6c58aa
Subham Das <subhamdassneha@gmail.com>
2022-12-19 03:49:49 +0530

    Update TopButtons.jsx

--- src/Components/TopButtons.jsx
+++ src/Components/TopButtons.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const TopButtons = () => {
-
-    const cities = [
-        {
-            id: 1,
-            title: 'London'
-        },
-        {
-            id: 2,
-            title: 'New Delhi'
-        },
-        {
-            id: 3,
-            title: 'Tokyo'
-        },
-        {
-            id: 4,
-            title: 'New York'
-        },
-        {
-            id: 5,
-            title: 'Paris'
-        }
-    ]
+const TopButtons = ({ setQuery }) => {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 2,
+      title: "New Delhi",
+    },
+    {
+      id: 3,
+      title: "Tokyo",
+    },
+    {
+      id: 4,
+      title: "New York",
+    },
+    {
+      id: 5,
+      title: "Paris",
+    },
+  ];
   return (
     <div className="flex item-center justify-around my-6">
-        {
-            cities.map((city)=>{
-                return (<button key={city.id} className="text-white text-lg font-medium">
-                            {city.title}
-                        </button>
-            )})
-        }
-        
+      {cities.map((city) => {
+        return (
+          <button
+            key={city.id}
+            className="text-white text-lg font-medium"
+            onClick={() => setQuery(city.title)}
+          >
+            {city.title}
+          </button>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default TopButtons
+export default TopButtons;
